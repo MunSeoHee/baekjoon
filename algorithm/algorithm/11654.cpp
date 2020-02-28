83b089ecf6b185cac344ed679b401ba8885a4077
ToA <nansh9815@naver.com>
2020-02-28 22:25:02 +0900

    2020.02.28

---
+++ algorithm/algorithm/11654.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	char x;
+	cin >> x;
+	cout << int(x);
+
+	system("PAUSE");
+
+
+	return 0;
+}
