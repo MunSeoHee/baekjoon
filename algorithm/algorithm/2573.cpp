d95132062b90490c51a0defdcea0bebc469d8211
ToA <nansh9815@naver.com>
2020-09-26 19:25:14 +0900

    20.09.26

--- algorithm/algorithm/2573.cpp
+++ algorithm/algorithm/2573.cpp
@@ -2,37 +2,93 @@
 #include <string>
 #include <algorithm>
 #include <vector>
-int route[][2] = { {-1,0},{0,-1},{0,1},{1,0} };
+#include <string.h>
+
+int route[4][2] = { {-1,0},{0,-1},{0,1},{1,0} };
+bool visit[301][301];
 using namespace std;
-vector<string>vec;
+int v[301][301];
+int N, M;
+
+void DFS(int init_x, int init_y) {
+	visit[init_x][init_y] = true;
+
+	for (int i = 0; i < 4; i++) {
+		int nx = init_x + route[i][0];
+		int ny = init_y + route[i][1];
+
+		if (nx < 1 || nx >= N - 1 || ny < 1 || ny >= M - 1) continue;
+		if (v[nx][ny] > 0 && !visit[nx][ny]) DFS(nx, ny);
+	}
+}
 
 void year() {
-	for (int i = 1; i < vec.size() - 1; i++) {
-		for (int j = 1; j < vec[i].size() - 1; j++) {
-			for (int z = 0;z < 4;z++) {
-
+	int vec[301][301];
+	for (int x = 0; x < N; x++)
+		for (int y = 0; y < M; y++)
+			vec[x][y] = v[x][y];
+	for (int i = 1; i < N - 1; i++) {
+		for (int j = 1; j < M - 1; j++) {
+			if (vec[i][j] != 0) {
+				int temp = 0;
+				for (int z = 0;z < 4;z++) {
+					if (vec[i + route[z][0]][j + route[z][1]] == 0) {
+						temp++;
+					}
+				}
+				if (v[i][j] - temp < 0) {
+					v[i][j] = 0;
+				}
+				else {
+					v[i][j] = v[i][j] - temp;
+				}
 			}
+			
 		}
 	}
 }
+
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	string str;
+	vector<int> t;
+	int result = 0;
+	cin >> N >> M;
 	
 
-	int N, M;
-	cin >> N >> M;
+	for (int i = 0; i < N;i++) {
+		for (int j = 0;j < M;j++) {
+			cin >> v[i][j];
+		}
+	}
 
-	while (N--)
-	{
-		getline(cin, str);
-		str.erase(remove_if(str.begin(), str.end(), isspace), str.end());
-		vec.push_back(str);
+	while (1) {
+		int island_count=0;
+		memset(visit, false, sizeof(visit));
+		bool check=false;
+		for (int i = 1; i < N - 1; i++) {
+			for (int j = 1; j < M - 1; j++) {
+				if (v[i][j] > 0 && !visit[i][j]) {
+					island_count++;
+					if (island_count >= 2) {
+						check = true;
+						break;
+					}
+					DFS(i, j);
+				}
+			}
+		}
+		if (check) break;
+		if (island_count == 0) {
+			cout << "0";
+			return 0;
+		}
+		year();	
+		result++;
+		
 	}
-	for (int i = 0; i < vec.size(); i++) {
-		cout << vec[i]<<endl;
-	}
+	cout << result;
+
 
 
 	system("PAUSE");
